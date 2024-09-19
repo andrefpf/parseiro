134f0565513321231c543d8b2ef4e82de94f25f9
André Fernandes <fpf.andre@gmail.com>
2024-09-19 16:09:40 -0300

    improve production rule and grammar visualization

--- parseiro/syntactic/grammar.py
+++ parseiro/syntactic/grammar.py
@@ -1,3 +1,4 @@
+from typing import Any
 from parseiro.syntactic.production_rule import ProductionRule
 
 
@@ -8,22 +9,40 @@ class GrammarVariable(str):
 
 class Grammar:
     def __init__(self) -> None:
-        self.production_rules = list()
+        self.__production_rules = list()
 
     def add_production_rule(self, origin, target):
-        p = ProductionRule(origin, target)
-        self.production_rules.append(p)
-
-    def __getitem__(self, key):
-        return GrammarVariable(key)
-
-    def __setitem__(self, key, val):
-        # Transform val into a tuple
+        # Transform target into a tuple
         try:
-            val = tuple(val)
+            target = tuple(target)
         except TypeError:
-            val = (val,)
-        self.add_production_rule(GrammarVariable(key), val)
-    
+            target = (target,)
+
+        p = ProductionRule(origin, target)
+        self.__production_rules.append(p)
+
+    def __getitem__(self, name: str) -> GrammarVariable:
+        return GrammarVariable(name)
+
+    def __setitem__(self, name: str, value: Any):
+        self.add_production_rule(GrammarVariable(name), value)        
+
     def __str__(self) -> str:
-        return "\n".join([str(i) for i in self.production_rules])
+        return "\n".join([str(i) for i in self.__production_rules])
+
+
+if __name__ == "__main__":
+    from parseiro.lexical.token import Token
+
+    class IdToken(Token):
+        pass
+
+    g = Grammar()
+    g["A"] = "a", g["A"]
+    g["A"] = "b"
+
+    # print(Token)
+    print(g)
+
+    # g._A = "a", g._A
+    # g._A = IdToken
--- parseiro/syntactic/production_rule.py
+++ parseiro/syntactic/production_rule.py
@@ -7,11 +7,16 @@ class ProductionRule:
     target: str
     
     def target_symbols(self):
-        return (i for i in self.target if not callable(i))
+        return (i for i in self.target)
 
     def non_empty_symbols(self):
         return (i for i in self.target_symbols() if not isinstance(i, int))
 
     def __str__(self) -> str:
-        targets = " ".join(str(i) for i in self.target_symbols())
+        def representation(x):
+            if type(x) == str:
+                return f'"{x}"'
+            return str(x)
+
+        targets = " ".join(representation(i) for i in self.target_symbols())
         return f"{self.origin} → {targets}"
